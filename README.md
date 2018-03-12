8a7cfa38dc85cf4ab77756de62bae1cf168b1f45
Brijesh <brijesh.reddy15@siesgst.ac.in>
2018-03-12 18:25:06 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# line-follower-bot
-An automated guided vehicle, which follow a visual line embedded on the floor or ceiling
+# Line Follower Bot
+An automated guided vehicle, which follow a visual line embedded on the floor.
+
+## How Does the Line Follower Robot Work?
+The working of a line follower robot is pretty straightforward. These robots have the capability to detect a black or dark line on a lighter surface depending on the contrast. They estimate whether the line underneath them is shifting toward their left or right as they move over them. Based on that estimation, they give respective signals to the motors to turn left or right so as to maintain a steady center with respect to the line.
+
+## Materials
+Components required are :
+
+1. 1 X NodeMCU
+2. 2 X IR Sensor Module
+3. 2 X Geared DC Motor
+4. 1 X Motor Driver Module
+5. 2 X Wheels
+6. 2 X Robot Chassis
+7. 1 X Castor Wheel
+8. Few Jumper Wires
+9. Nut-Bolts
+10. 1 X 9V-battery / Power Bank (For efficient performance)
+
+Assemble the robot using the above-mentioned components. Fix the IR sensor module at appropriate height and distance on the Chassis of the robot. Burn the Above code on Arduino UNO using Arduino IDE & change the value of threshold in your program accordingly so that the sensors are able to differentiate between the white part & the black line of your track. For better performance & longer battery use Power Bank.
+
+## License
+The software licensed under the [MIT license](LICENSE).
---
+++ line_follower_bot/line_follower_bot.ino
@@ -0,0 +1,66 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(12, OUTPUT);    // Motor Pin 1
+  pinMode(13, OUTPUT);
+
+  pinMode(14, OUTPUT);    // Motot Pin 2
+  pinMode(15, OUTPUT);
+
+  pinMode(4, INPUT);      // Sensor Pin 1
+  pinMode(5, INPUT);      // Sensor Pin 2
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // LINE FOLLOWER CODE
+
+  int ls = digitalRead(4);
+  int rs = digitalRead(5);
+
+  Serial.print("Sensor 1 value: ");   // Display the sensor 1 value
+  Serial.println(ls);
+  Serial.print("Sensor 2 value: ");   // Display the sensor 2 value
+  Serial.println(rs);
+
+  // To move the bot forward
+  if ((ls == HIGH) && (rs == HIGH)) {
+    digitalWrite(12, HIGH);
+    digitalWrite(13, LOW);
+    digitalWrite(14, HIGH);
+    digitalWrite(15, LOW);
+    Serial.print("Moving forward...");
+    delay(2000);
+  }
+
+  // To turn right
+  if ((ls == HIGH) && (rs == LOW)) {
+    digitalWrite(12, HIGH);
+    digitalWrite(13, LOW);
+    digitalWrite(14, LOW);
+    digitalWrite(15, LOW);
+    Serial.print("Turning right...");
+    delay(2000);
+  }
+
+  // To turn left
+  if ((ls == LOW) && (rs == HIGH)) {
+    digitalWrite(12, LOW);
+    digitalWrite(13, LOW);
+    digitalWrite(14, LOW);
+    digitalWrite(15, HIGH);
+    Serial.print("Turning left...");
+    delay(2000);
+  }
+
+  // To stop
+  if ((ls == LOW) && (rs == LOW)) {
+    digitalWrite(12, LOW);
+    digitalWrite(13, LOW);
+    digitalWrite(14, LOW);
+    digitalWrite(15, LOW);
+    Serial.print("STOPPING...");
+    delay(2000);
+  }
+}
